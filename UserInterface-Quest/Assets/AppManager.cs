61a1f28495ae59128d27911e246c9362e909d161
Gary Robinson <gary.robinson2524@gmail.com>
2020-07-13 11:42:15 -0400

    Added Text Canvas with message

---
+++ UserInterface-Quest/Assets/AppManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    private GameObject messageText;
+
+    private void Awake()
+    {
+        messageText = GameObject.Find("MessageText");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        messageText.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
